954ebdc03b7a861d4e74e1dc1e8dd40851a7fd5d
noewal <noewal91@gmail.com>
2020-12-13 19:42:46 +0900

    uploader モーデルにバルテーションを設定

--- app/models/uploader.rb
+++ app/models/uploader.rb
@@ -1,2 +1,14 @@
 class Uploader < ApplicationRecord
-end
+
+  #photo validation
+  has_attached_file :photo, styles: {large: "450x450>", thumb: "50x50#"}
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+  
+  #music validation
+  has_attached_file :music, validate_media_type: false
+  validates_attachment :music,
+  :content_type => {:content_type => ["audio/mpeg", "audio/mp3"]},
+  :file_type => {:matches => [/mp3\Z/]}
+  
+  end
+  
